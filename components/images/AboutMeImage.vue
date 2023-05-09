a1e7c4883b562a8dcebeaae99e6835814e4fb35a
Francisco Pérez <devfcph@gmail.com>
2023-05-09 16:56:38 -0600

    Update path image about me

--- components/images/AboutMeImage.vue
+++ components/images/AboutMeImage.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="img-about">
-    <img class="img-fluid" v-bind:src="imgMe" alt="abou me" />
+    <img class="img-fluid" :src="imgMe" alt="abou me" />
   </div>
 </template>
 
 <script>
 export default {
-    computed: {
-        imgMe() {
-            //'../static/assets/img/about/1082318.jpg', 
-            return 'https://via.placeholder.com/824x1024';
-        }
-    }
+  computed: {
+    imgMe() {
+      return `${require(`@/static/assets/img/about/1082318.jpg`)}`;
+      //Descomentar para visualizar foto genérica
+      //return 'https://via.placeholder.com/824x1024';
+    },
+  },
 };
 </script>
