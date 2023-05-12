4cfe52634abc7ca1a0b309954b2807278daa85a3
Cristóbal Pérez <cris@Ubutu.myguest.virtualbox.org>
2023-05-12 17:26:29 -0600

    Add image const object

--- helpers/constants.js
+++ helpers/constants.js
@@ -6,3 +6,8 @@ export const typeOfIcon = {
     PHONE: "icon-screen-smartphone",
     SKYPE: "icon-social-skype"
 }
+
+export const imagePath = Object.freeze({
+    TESTIMONIAL_ONE: "https://via.placeholder.com/50x50",
+    
+});
