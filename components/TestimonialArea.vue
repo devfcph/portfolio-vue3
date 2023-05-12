46c79b0a34e85bcb7a0dca4e35343d989ab2a201
Cristóbal Pérez <cris@Ubutu.myguest.virtualbox.org>
2023-05-12 17:27:28 -0600

    Add testimonial components

--- components/TestimonialArea.vue
+++ components/TestimonialArea.vue
@@ -7,79 +7,11 @@
           <!-- start testimonial carousel -->
           <div class="test-slider">
             <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur leo est, feugiat nec elementum id, suscipit id nulla.
-                Nulla sit amet luctus dolor. Etiam finibus consequat ante ac
-                congue. Quisque porttitor porttitor tempus.
-              </p>
-              <div class="author-info">
-                <img src="https://via.placeholder.com/50x50" alt="client" />
-                <h4>Soura</h4>
-                <div class="position">
-                  Project Manager
-                  <span>Karmen</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur leo est, feugiat nec elementum id, suscipit id nulla.
-                Nulla sit amet luctus dolor. Etiam finibus consequat ante ac
-                congue. Quisque porttitor porttitor tempus.
-              </p>
-              <div class="author-info">
-                <img src="https://via.placeholder.com/50x50" alt="client" />
-                <h4>Sara Ezzat</h4>
-                <div class="position">
-                  Project Manager
-                  <span>Ask</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur leo est, feugiat nec elementum id, suscipit id nulla.
-                Nulla sit amet luctus dolor. Etiam finibus consequat ante ac
-                congue. Quisque porttitor porttitor tempus.
-              </p>
-              <div class="author-info">
-                <img src="https://via.placeholder.com/50x50" alt="client" />
-                <h4>Ahmed Ezzat</h4>
-                <div class="position">
-                  Project Manager
-                  <span>Twitter</span>
-                </div>
-              </div>
-            </div>
-            <!-- end tetimonial item -->
-
-            <!-- start tetimonial item -->
-            <div class="test-item">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur leo est, feugiat nec elementum id, suscipit id nulla.
-                Nulla sit amet luctus dolor. Etiam finibus consequat ante ac
-                congue. Quisque porttitor porttitor tempus.
-              </p>
-              <div class="author-info">
-                <img src="https://via.placeholder.com/50x50" alt="client" />
-                <h4>Karim Ezzat</h4>
-                <div class="position">
-                  Project Manager
-                  <span>Facebook</span>
-                </div>
-              </div>
-            </div>
+            <TestimonialItem
+              v-for="(testimonial, index) in testimonials"
+              v-bind:key="index"
+              v-bind:testimonial="testimonial"
+            ></TestimonialItem>
             <!-- end tetimonial item -->
           </div>
           <!-- end testimonial carousel -->
@@ -91,7 +23,33 @@
 </template>
 
 <script>
+import { imagePath } from "../helpers/constants";
+import TestimonialItem from "./global/TestimonialItem.vue";
+
 export default {
-  name: "TestimonialArea"
+  name: "TestimonialArea",
+  components: {
+    TestimonialItem,
+  },
+  data() {
+    return {
+      testimonials: [
+        {
+          name: "ABDVSDVS",
+          position: "BSKDK KJHASD KF",
+          company: "ksahf akuef",
+          text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: imagePath.TESTIMONIAL_ONE,
+        },
+        {
+          name: "ABDVSDVS",
+          position: "BSKDK KJHASD KF",
+          company: "ksahf akuef",
+          text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+          image: imagePath.TESTIMONIAL_ONE,
+        },
+      ],
+    };
+  },
 };
 </script>
---
+++ components/global/TestimonialItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="test-item">
+    <SimpleText :text="testimonial.text"></SimpleText>
+    <div class="author-info">
+      <img :src="testimonial.image" alt="client" />
+      <h4>{{ testimonial.name }}</h4>
+      <div class="position">
+        {{ testimonial.position }}
+        <span> {{ testimonial.company }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SimpleText from "../labels/SimpleText.vue";
+
+export default {
+  components: {
+    SimpleText,
+  },
+  props: {
+    testimonial: Object,
+  },
+  computed: {
+    getTestimonialImage() {
+      //return `${require(`@` + this.testimonial.image)}`;
+      return `${require(this.testimonial.image)}`;
+    },
+  },
+};
+</script>
