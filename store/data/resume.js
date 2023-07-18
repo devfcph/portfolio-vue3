a95ca2fbbc38112fa05aecbb14096cd593c6f4c2
1082318 <francisco.perezh@elektra.com.mx>
2023-07-18 15:27:31 -0600

    Add resume storage file

---
+++ store/data/resume.js
@@ -0,0 +1,212 @@
+const { fontType } = require("../../helpers/constants");
+
+export const Experiences = Object.freeze({
+    GRUPO_SALINAS: {
+        date: "Oct 2022 - Actuallity PERROOOO",
+        city: "Mexico City. México",
+        title: "Microservices Developer",
+        place: "Grupo Salinas [Hybrid]",
+        body: [
+            { info: "Skills:", style: fontType.BOLD },
+            { info: "✅ Java | SpringBoot " },
+            { info: "✅ Kafka" },
+            { info: "✅ CI/CD with Jenkins" },
+            { info: "✅ API Gateway with ApiGee" },
+        ],
+    },
+    GONET: {
+        date: "Jul 2022 - Oct 2022",
+        title: "iOS Mobile Developer",
+        place: "Gonet [Remote]",
+        body: [
+            { info: "Skills:", style: fontType.BOLD },
+            { info: "✅ Swift " },
+        ],
+    },
+    HACIENDA_CHIAPAS: {
+        date: "Oct 2020 - Oct 2022",
+        city: "Tuxtla Gutiérrez, Chiapas. México",
+        title: "Fullstack developer",
+        place: "Secretaría de Hacienda del Estado de Chiapas [On site]",
+        body: [
+            { info: "Skills:", style: fontType.BOLD },
+            { info: "✅ Microsoft SQL Server | Stored Procedures, Functions, Jobs " },
+            { info: "✅ .NET | Microservices, Web API " },
+            { info: "✅ VueJs | FrontEnd Apps" },
+        ],
+    },
+    GLOBAL_TRANSFER: {
+        date: "Jul 2020 - Oct 2020",
+        city: "Tuxtla Gutiérrez, Chiapas. México",
+        title: "Fullstack developer",
+        place: "Global Transfer [On site]",
+        body: [
+            { info: "Skills:", style: fontType.BOLD },
+            { info: "✅ Microsoft SQL Server | Stored Procedures, Functions, Jobs " },
+            { info: "✅ C# | Restful Web Services" },
+            { info: "✅ Flutter | Mobile App" },
+            { info: "✅ Laravel | Web App" },
+        ],
+    },
+    INDUXSOFT: {
+        date: "Ago 2018 - Jan 2020",
+        city: "Tuxtla Gutiérrez, Chiapas. México",
+        title: "Fullstack developer",
+        place: "Induxsoft Data Services [On site]",
+        body: [
+            { info: "Skills:", style: fontType.BOLD },
+            { info: "✅ SQL Server | Stored Procedures, Functions, Jobs " },
+            { info: "✅ MySQL | Stored Procedures, Functions, Jobs " },
+            { info: "✅ C# | Console Applicatios, Monolithic Applications, SOAP Web services" },
+            { info: "✅ Javascript | Web apps" },
+            { info: "✅ PHP | Web Apps" },
+            { info: "✅ VB | Monolithic Applications" },
+            { info: "✅ Bootstrap | Web Apps " },
+        ],
+    },
+});
+
+
+export const Schools = Object.freeze({
+    UNACH: {
+        date: "2016 - 2022",
+        city: "Tuxtla Gutiérrez, Chiapas. México",
+        title: "Software Development and Technology Engineering",
+        place: "Autonomous University of Chiapas",
+        body: [
+            { info: "Main topics: ", style: fontType.BOLD },
+            { info: "🔷  Object Oriented Programming [OOP]" },
+            { info: "🔷  Structured programming" },
+            { info: "🔷  Database Management" },
+            { info: "🔷  SOAP & REST services" },
+            { info: "🔷  Robotics & AI" },
+            { info: "🔷  Project management" },
+            { info: "🔷  Web Apps" },
+        ],
+    },
+    UNIBOYACA: {
+        date: "January 2020 - June 2020",
+        city: "Tunja, Boyacá. Colombia",
+        title: "Interchange Student",
+        place: "University of Boyacá",
+        body: [
+            { info: "I got a scholarship to study in Boyacá, Colombia." },
+            {
+                info: "Main learnings obtained during the school interchange period:",
+                style: fontType.BOLD,
+            },
+            {
+                info: "🔷  Programation with Python as a language for Big Data.",
+            },
+            { info: "🔷  SQL as a tool to store big data." },
+            {
+                info: "🔷  Project management with agile software development methodologies.",
+            },
+        ],
+    },
+});
+
+
+export const HardSkills = Object.freeze({
+    DATA: {
+        1: {
+            title: "xdfxds",
+            percent: {
+                value: "50%",
+                isVisible: false,
+            },
+        },
+        2: {
+            title: "A B C D E F G H",
+            percent: {
+                value: "50%",
+                isVisible: false,
+            },
+        },
+        3: {
+            title: "A B C D E F G H",
+            percent: {
+                value: "50%",
+                isVisible: true,
+            },
+        },
+        4: {
+            title: "A B C D E F G H",
+            percent: {
+                value: "50%",
+                isVisible: false,
+            },
+        },
+    }
+});
+
+export const SoftSkills = Object.freeze({
+    DATA: {
+        1: {
+            title: "SOFT XDXDDX",
+            percent: {
+                value: "50%",
+                isVisible: false,
+            },
+        },
+        2: {
+            title: "SOFT XDXDX",
+            percent: {
+                value: "50%",
+                isVisible: false,
+            },
+        },
+    }
+});
+
+
+export const Courses = Object.freeze([
+    {
+        date: "January 2020 - June 2020",
+        title: "NOMBRE DEL CURSO",
+        place: "PLATZI ACADEMY dsdfsdf",
+        body: "CHEEMS xdxdxdxxd",
+    },
+    {
+        date: "January 2020 - June 2020",
+        title: "NOMBRE DEL CURSO",
+        place: "PLATZI ACADEMY",
+        body: "CHEEMS iusadfhludsa",
+    },
+    {
+        date: "January 2020 - June 2020",
+        title: "NOMBRE DEL CURSO",
+        place: "PLATZI ACADEMY",
+        body: "CHEEMS",
+    },
+    {
+        date: "January 2020 - June 2020",
+        title: "NOMBRE DEL CURSO",
+        place: "PLATZI ACADEMY",
+        body: "CHEEMS",
+    },
+    {
+        date: "January 2020 - June 2020",
+        title: "NOMBRE DEL CURSO",
+        place: "PLATZI ACADEMY",
+        body: "CHEEMS",
+    },
+    {
+        date: "January 2020 - June 2020",
+        title: "NOMBRE DEL CURSO",
+        place: "PLATZI ACADEMY",
+        body: "CHEEMS",
+    },
+    {
+        date: "January 2020 - June 2020",
+        title: "NOMBRE DEL CURSO",
+        place: "PLATZI ACADEMY",
+        body: "CHEEMS",
+    },
+    {
+        date: "January 2020 - June 2020",
+        title: "NOMBRE DEL CURSO",
+        place: "PLATZI ACADEMY",
+        body: "CHEEMS",
+    },
+]);
