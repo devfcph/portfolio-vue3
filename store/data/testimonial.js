061a51e614f4d2a84cffb14f7d52d7fb0586111c
1082318 <francisco.perezh@elektra.com.mx>
2023-08-15 12:23:39 -0600

    [test]

--- store/data/testimonial.js
+++ store/data/testimonial.js
@@ -4,7 +4,7 @@ export const testimonials = Object.freeze([
     {
         name: "Raul Vázquez",
         position: "FULLSTACK DEVELOPER | RPA DEVELOPER",
-        company: "MOSBIT | EMERGYS",
+        company: "MOSBIT | EMERGYS ",
         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
         externalLink: "https://www.linkedin.com/in/valr090698",
         image: imagePath.RAUL_VAZQUEZ,
